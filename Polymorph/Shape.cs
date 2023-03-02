912032308db38f0af867d3dad1f67512b11f60f5
LuisMartz <114309908+LuisMartz@users.noreply.github.com>
2023-03-02 11:03:29 +0100

    Add files via upload

---
+++ Polymorph/Shape.cs
@@ -0,0 +1,13 @@
+abstract class Shape
+{
+    private string color;
+    public Shape(string color)
+    {
+        this.color = color;
+    }
+    public string GetColor()
+    {
+        return color;
+    }
+    public abstract double GetArea();
+}
